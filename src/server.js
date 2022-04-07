422e25b59f8581f2ec172909e495a53cd3fc3fd3
Bruno Eduardo <91034858+brunoedubems@users.noreply.github.com>
2022-04-07 15:50:47 -0300

    server

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+//Dados
+
 const proffys = [
     {name: "Diego Fernandes", 
     avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4", 
@@ -19,23 +21,63 @@ const proffys = [
 }
 ]
 
+const subjects = [
+"Artes",
+"Biologia",
+"Ciências",
+"Educação Física",
+"Física",
+"Geografia",
+"História",
+"Matemática",
+"Português",
+"Química",
+
+]
+
+const weekdays = [
+"Domingo",
+"Segunda-feira",
+"Terça-feira",
+"Quarta-feira",
+"Quinta-feira",
+"Sexta-feira",
+"Sábado",
+    ]
+
+//funcionalidades
 function pageLanding(req, res){
-    return res.sendFile(__dirname + "/views/index.html")  
+    // return res.render(__dirname + "/views/index.html")  
+    return res.render("index.html")  
 }
 
 function pageStudy(req, res){
-    return res.sendFile(__dirname + "/views/study.html")
+    // return res.render(__dirname + "/views/study.html")
+    const filters = req.query
+    return res.render("study.html", {proffys, subjects, weekdays})
 }
 
 function pageGiveClasse(req, res){
-    return res.sendFile(__dirname + "/views/give-classes.html")
+const dados = req.query
+//adicionar dados a lista de proffys
+
+    // return res.render(__dirname + "/views/give-classes.html")
+    return res.render("give-classes.html", {subjects, weekdays})
 }
 
+
+//servidor
 const express = require('express')
 const server = express()
 const nunjucks = require('nunjucks')
 
+//configurar nunjucks(template engine)
+nunjucks.configure('src/views', {
+    express: server,
+    noCache: true,
+})
 
+//Inicio e configuração do servidor
 server
 //configurar arquivos estáticos (css, scripts, imagens)
 .use(express.static("public"))
@@ -43,5 +85,6 @@ server
 .get("/", pageLanding)
 .get("/study", pageStudy)
 .get("/give-classes", pageGiveClasse)
+//start do servidor
 .listen(5500)
 
